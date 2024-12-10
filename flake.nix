a58612bd1130223b75479242bffa66b0ecc75837
Antonio Yang <yanganto@gmail.com>
2024-12-10 10:36:48 +0800

    add cache

--- flake.nix
+++ flake.nix
@@ -29,6 +29,18 @@
     };
   };
 
+  nixConfig.extra-substituters = [
+    "https://nix-community.cachix.org"
+    "https://nix-cache.ant-lab.tw"
+    "https://cache.garnix.io"
+  ];
+  nixConfig.extra-trusted-public-keys = [
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    "nix-cache.ant-lab.tw:zIdryBfFvXk6AyoaN8P5WWFELzDWOK7bQvIzl8nL5Y8="
+  ];
+  nixConfig.connect-timeout = 1;
+
   outputs = { self, nixpkgs, rust-overlay, flake-utils, sui-flake, ... }:
     flake-utils.lib.eachDefaultSystem (system: {
         devShells.default = sui-flake.devShells."${system}".slim;
